eaa8cb4c8c2ea64e8f4b218ab4877307232280fa
Betty <betty.spurgeon713@gmail.com>
2022-06-14 16:24:11 +0200

    Register User form and route
    View and route created for the Register User option (GET) only.

---
+++ resources/views/register-user.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="first-name"><br>
+        <input type="text" name="last-name"><br>
+        <input type="email" name="email"><br>
+        <input type="password" name="password"><br>
+        <select name="type" id="">
+            <option value="seller">Seller</option>
+            <option value="buyer">Buyer</option>
+            <option value="renter">Renter</option>
+            <option value="landlord">Landlord</option>
+        </select><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
