62a37c2f1ca6a05be3f8925efccb8275b443ac52
Betty <betty.spurgeon713@gmail.com>
2022-06-15 13:25:03 +0200

    user registration authentication bug fix
    Errors now appear in the log in page and the sign up form now requires that email and username be completely unique in the database.

--- resources/views/register-user.blade.php
+++ resources/views/register-user.blade.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    
 
+<body>
+    <div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+    </div>
     <form action="" method="POST">
         @csrf
         <input type="text" name="first_name" placeholder="first name"><br>
@@ -26,4 +35,5 @@
         <input type="submit" value="Register">
     </form>
 </body>
+
 </html>
