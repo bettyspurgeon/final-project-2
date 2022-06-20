5d2800c1703fdd02e3c8c65e7c5334721164bcf5
Lucas Souza <soker.souza@gmail.com>
2022-06-20 13:51:45 +0200

    Header responsive and search bar adjusted

---
+++ public/js/template.js
@@ -0,0 +1,16 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+
+function toggleMenu() {
+  var menuBox = document.getElementById('menu-box');    
+  if(menuBox.style.display == "flex") {
+    menuBox.style.display = "none";
+  }
+  else {
+    menuBox.style.display = "flex";
+  }
+}
+
