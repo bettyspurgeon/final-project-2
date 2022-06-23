b836f99840523a5e078686499af94017474c81b7
Betty <betty.spurgeon713@gmail.com>
2022-06-23 13:52:01 +0200

    Fix page that displays all properties
    Also did a minor fix on the template - I centred the buttons on the hidden menu :)

--- resources/views/property-views/properties.blade.php
+++ resources/views/property-views/properties.blade.php
@@ -3,52 +3,30 @@
 @section('title', 'Properties List')
 
 @section('content')
-    @foreach ($properties as $property)
-        <div class="big-container">
-
-            <div class="properties-container">
-                <div class="properties-picture" style="background-image: url('uploads/{{ $property->pictures }}')">
+    <section class="property-cards-container">
+        @foreach ($properties as $property)
+            <div class="property-card">
+                <div class="property-picture" style="background-image: url('uploads/{{ $property->pictures }}')">
                 </div>
-                <div class="properties-description">
-                    <div class="wrapper-1">
+                <div class="property-info-container">
+                    <p>Type: {{ ucfirst($property->type) }}</p>
+                    <p> Price:
+                        {{ $property->purpose == 'rent' ? $property->price . '€/Per Month' : $property->price . '€' }}</p>
 
-                        <strong>Type : </strong>
-                        <p class="p-description"> {{ $property->type }}</p><br>
-                        <strong>Price : </strong>
-                        <p class="p-description"> {{ $property->price }}</p><br>
-                        <strong>Location : </strong>
-                        <p class="p-description"> {{ $property->location }}</p><br>
-                        <strong>Date_avaliable: </strong>
-                        <p class="p-description"> {{ $property->date_avaliable }}</p><br>
-                        <strong>Area: </strong>
-                        <p class="p-description"> {{ $property->area }}</p><br>
-                    </div>
-                    <div class="wrapper-2">
-                        <strong>Bedrooms : </strong>
-                        <p class="p-description"> {{ $property->bedrooms }}</p><br>
-                        <strong>Bathrooms : </strong>
-                        <p class="p-description"> {{ $property->bathrooms }}</p><br>
-                        <strong>Children: </strong>
-                        <p class="p-description"> {{ $property->children }}</p><br>
-                        <strong>Pets: </strong>
-                        <p></p class="p-description"> {{ $property->pets }}</p><br>
-                        <strong>Descripation : </strong>
-                        <p class="p-description"> {{ $property->description }}</p><br>
+                    <p>Location: {{ $property->location }}</p>
 
-
-                    </div>
-
+                    <p> Date Avaliable: {{ $property->date_avaliable }}</p><br>
+                    
+                        <a class="details-link" href="{{ route('properties.details', [$property->id]) }}">Details</a>
+                  
                 </div>
 
-                <div class="properties-links">
-                    <a class="properties-a-link" href="{{ route('properties.details', [$property->id]) }}">Details</a>
-                </div>
-                <div class="landlord_preferences">
+                {{-- <div class="landlord_preferences">
                     <h4>Landlord Request</h4>
                     <strong>Contract : </strong> {{ $property->contract }}<br>
                     <strong>Income : </strong> {{ $property->income }}<br>
-                </div>
+                </div> --}}
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </section>
 @endsection
