95d8a9276e09f6f2c13641afcd64167e0886634f
Lucas Souza <soker.souza@gmail.com>
2022-06-26 11:23:24 +0200

    Logo with typewriting effect and favicon

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -17,27 +17,31 @@
 
 
 
-
 console.clear();
 function typingEffect(element,speed){
   let text=element.innerHTML;
   element.innerHTML="";
- var i=0;
-  var timer=setInterval(function(){
+  let i=0;
+  let timer=setInterval(function(){
     if(i<text.length){
       element.append(text.charAt(i))
       i++;
     }else{
       clearInterval(timer);
+
     }
   },speed)
   
 }
 
+// typingEffect(element, speed);
 
 
-const h1=document.querySelector('h1');
-typingEffect(h1,150);
+
+
+
+const h1 = document.querySelector('h1');
+typingEffect(h1,200);
 
 
 
