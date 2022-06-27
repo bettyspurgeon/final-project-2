d5221e855604a08b3f134c57f06c6e976353214f
Elisângela <elisangela.semedo@gmail.com>
2022-06-27 05:26:33 +0200

    Forms-correction

--- resources/views/user-pages/renter-file-upload.blade.php
+++ resources/views/user-pages/renter-file-upload.blade.php
@@ -29,7 +29,6 @@
                 </div>
             @endif
 
-<<<<<<< HEAD
             @if (session('error'))
                 <div class="alert alert-success" style="color: red">
                     {{ session('error') }}
@@ -49,24 +48,6 @@
                 <input class="forminput" type="number" name="income" id="income">
                 <p>Please Provide a Document that confirms your contract type and Income.</p>
                 <input type="file" name="renter_document"><br>
-=======
-    <form class="form" action="" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="contract-type">Select your contract type:</label>
-        <select name="contract_type" id="contract-type">
-            <option selected value=></option>
-            <option value="CDI">CDI</option>
-            <option value="CDD">CDD</option>
-            <option value="none">No Contract</option>
-        </select><br>
-        <label for="income">Provide your monthly income</label>
-        <input class="forminput" type="number" name="income" id="income">
-        <p>Please Provide a Document that confirms your contract type and Income.</p>
-        <input type="file" name="renter_document"><br>
-
-        <input class="forminput" type="submit" name="submitBtn" value="Send the file">
-    </form>
->>>>>>> main
 
                 <input class="forminput" id="btn" type="submit" name="submitBtn" value="Send the file">
             </form>
